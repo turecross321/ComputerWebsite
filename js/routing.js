69a814971b0fa45144b2725e24b1af7a75589df6
Ture <51852312+turecross321@users.noreply.github.com>
2023-10-06 17:00:40 +0200

    Implement javascript routing

---
+++ js/routing.js
@@ -0,0 +1,31 @@
+let navButtons;
+let contentParent;
+
+async function init() {
+    navButtons = document.getElementsByClassName("nav-button");
+    contentParent = document.getElementById("content-parent");
+    await load_pages();
+    goToPage(0);
+}
+
+const pageContentsDir = "../page_contents/";
+let pageContents = [];
+async function load_pages() {
+    for (let i = 0; i < navButtons.length; i++) {
+        const response = await fetch(pageContentsDir + i + ".html")
+        const html = await response.text();
+        pageContents.push(html);
+    }
+
+    return;
+}
+
+let currentPage = 0;
+function goToPage(pageIndex) {
+    navButtons[currentPage].classList.remove("nav-button-selected");
+
+    currentPage = pageIndex;
+    contentParent.innerHTML = pageContents[currentPage];
+
+    navButtons[currentPage].classList.add("nav-button-selected");
+}
