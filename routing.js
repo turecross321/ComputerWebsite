d46d57f8d0dea40219b1018af8b3488f12e3c951
turecross321 <51852312+turecross321@users.noreply.github.com>
2023-10-06 17:56:49 +0200

    Add no file access error

--- routing.js
+++ routing.js
@@ -12,22 +12,25 @@ async function init() {
 }
 
 const pageContentsDir = "page_contents/";
-let pageContents = [];
+let pageContents = []
 async function load_pages() {
     for (let i = 0; i < navButtons.length; i++) {
-        const response = await fetch(pageContentsDir + i + ".html")
-        const html = await response.text();
-        pageContents.push(html);
+        try {
+            const response = await fetch(pageContentsDir + i + ".html");
+            const html = await response.text();
+            pageContents.push(html);
+        } catch (e) {
+            // add error page
+            pageContents = ["<html lang='sv'><main id=\"primary-main\"><h2>Katastrof</h2><p>Routern verkar ha misslyckats med att nå sido-innehållet. Detta har troligtvis hänt för att du har öppnat \"index.html\" filen lokalt, och din webläsare därför inte ger hemsidan tillgång till innehållen. Detta kan du fixa genom att hosta hemsidan på en server, eller genom att starta din webläsare med \"--allow-file-access-from-files\"-flaggan. </p></main></html>"];
+        }
     }
-
-    return;
 }
 
 let currentPage = 0;
 function goToPage(pageIndex) {
     navButtons[currentPage].classList.remove("nav-button-selected");
 
-    currentPage = pageIndex;
+    currentPage = Math.min(pageIndex, pageContents.length - 1);
     contentParent.innerHTML = pageContents[currentPage];
     document.title = originalDocTitle + " - " + navButtons[currentPage].innerHTML;
 
