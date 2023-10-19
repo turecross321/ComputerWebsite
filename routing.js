0fbf1f642eba8bcbb8a4b303fa13661db53030c9
Ture <51852312+turecross321@users.noreply.github.com>
2023-10-19 11:08:13 +0200

    Preload images

--- routing.js
+++ routing.js
@@ -9,14 +9,36 @@ async function init() {
 
     contentParent.classList.add("hidden");
     await load_pages();
+    preload_images();
     goToPage(0);
-    animateContentParent("fade-in");
+
     contentParent.classList.remove("hidden");
 }
 
-function animateContentParent(className) {
-    contentParent.classList.remove("fade-in");
-    contentParent.classList.add(className);
+
+function preload_images() {
+    const urls = get_image_urls();
+    for (let url of urls) {
+        preload_image(url);
+    }
+}
+
+function get_image_urls() {
+    image_urls = [];
+    for (page of pageContents) {
+        images = page.getElementsByTagName('img');
+
+        for (let image of images) {
+            image_urls.push(image.src);
+        }
+    };
+
+    return image_urls;
+}
+
+function preload_image(url) {
+    let image = new Image();
+    image.src = url;
 }
 
 const pageContentsDir = "page_contents/";
@@ -25,13 +47,20 @@ async function load_pages() {
     for (let i = 0; i < navButtons.length; i++) {
         try {
             const response = await fetch(pageContentsDir + i + ".html");
-            const html = await response.text();
+            const html = textToHtml(await response.text());
             pageContents.push(html);
         } catch (e) {
             // add error page
-            pageContents = ["<html lang='sv'><main id=\"primary-main\"><h2>Katastrof</h2><p>Routern verkar ha misslyckats med att nå sido-innehållet. Detta har troligtvis hänt för att du har öppnat \"index.html\" filen lokalt, och din webläsare därför inte ger hemsidan tillgång till innehållen. Detta kan du fixa genom att hosta hemsidan på en server, eller genom att starta din webläsare med \"--allow-file-access-from-files\"-flaggan. </p></main></html>"];
+            const error = "<html lang='sv'><main id=\"primary-main\"><h2>Katastrof</h2><p>Routern verkar ha misslyckats med att nå sido-innehållet. Detta har troligtvis hänt för att du har öppnat \"index.html\" filen lokalt, och din webläsare därför inte ger hemsidan tillgång till innehållen. Detta kan du fixa genom att hosta hemsidan på en server, eller genom att starta din webläsare med \"--allow-file-access-from-files\"-flaggan. </p></main></html>";
+            pageContents.push(error);
         }
     }
+}
+
+function textToHtml(text) {
+    var html = document.createElement('div');
+    html.innerHTML = text;
+    return html;
 }
 
 let currentPage = 0;
@@ -39,7 +68,7 @@ function goToPage(pageIndex) {
     navButtons[currentPage].classList.remove("nav-button-selected");
 
     currentPage = Math.min(pageIndex, pageContents.length - 1);
-    contentParent.innerHTML = pageContents[currentPage];
+    contentParent.innerHTML = pageContents[currentPage].innerHTML;
     document.title = originalDocTitle + " - " + navButtons[currentPage].innerHTML;
 
     navButtons[currentPage].classList.add("nav-button-selected");
